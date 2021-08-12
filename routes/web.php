1f294e6668f3e27c6502493ff5482243676fc0c8
githubarj <85483196+githubarj@users.noreply.github.com>
2021-08-12 07:36:20 +0300

    the route still bringgs 404 to be fixed

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,8 +15,11 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 Route::middleware(['auth', 'isAdmin'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('admin.index');
+    Route::get('/dashboard', 'Admin\FrontendController@index');
+
+    Route::get('categories', 'Admin\CategoryController@index');
+    Route::get('add-category', 'Admin\CategoryController@add');
+    Route::post('insert-category', 'Admin\CategoryController@insert');
     });
 
-});
+
